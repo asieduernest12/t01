266339721d78693dc7260d46cb80f88d16433273
alfa <manalpha19@gmail.com>
2022-04-25 18:16:49 -0500

    show errors with ini

--- gpx_maker.php
+++ gpx_maker.php
@@ -3,6 +3,9 @@
 // Report all PHP errors
 error_reporting(E_ALL);
 
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
 // phpinfo();
 // mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
